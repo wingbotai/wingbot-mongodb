2f6e68c5f2c88b45d8dcc26a2fce9ee90a6b0bab
David Menger <david.menger@me.com>
2018-05-27 21:30:57 +0200

    Fixed exports

--- src/main.js
+++ src/main.js
@@ -6,9 +6,11 @@
 const StateStorage = require('./StateStorage');
 const BotTokenStorage = require('./BotTokenStorage');
 const ChatLogStorage = require('./ChatLogStorage');
+const BotConfigStorage = require('./BotConfigStorage');
 
 module.exports = {
     StateStorage,
     BotTokenStorage,
-    ChatLogStorage
+    ChatLogStorage,
+    BotConfigStorage
 };
